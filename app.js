bc5c849c1baf0dc8cca0cc82f6132a51560ec3f4
Oleg Seriaga <oleg@targetprocess.com>
2019-02-19 16:34:32 +0300

    formatting

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const resolve = require('path').resolve;
-
+const Airtable = require('airtable');
+const JSONStream = require('JSONStream');
 const app = express();
+const VALID_API_KEY = `Provide valid Airtable API Key.`;
 app.use(bodyParser.json());
 
 const getAuthentication = () => [{
@@ -33,7 +35,8 @@ const getSources = () => [
         filter: [{
             id: 'base',
             name: 'Base Id',
-            description: 'Navigate to https://airtable.com/api. Click on desired base and copy {base id}. It can be found in browser address bar https://airtable.com/{base id}/api/docs#curl/introduction',
+            description: `Navigate to https://airtable.com/api. Click on desired base and copy {base id}. 
+                It can be found in browser address bar https://airtable.com/{base id}/api/docs#curl/introduction`,
             type: 'text'
         },
             {
@@ -58,17 +61,65 @@ app.get('/', (req, res) => {
     res.json(appConfig);
 });
 
-
-app.get('/logo', (req, res) =>  res.sendFile(resolve('./logo.svg')));
+app.get('/logo', (req, res) => res.sendFile(resolve('./logo.svg')));
 
 app.post('/validate', (req, res) => {
     if (!req.body.fields || !req.body.fields.key) {
         throw new Error('Please provide Airtable API Key');
     }
     const key = req.body.fields.key;
-    const ending = key.substr(key.length - 4, 4);
+    const endingForAccountName = key.substr(key.length - 3, 3);
     //Authentication check is possible only on retrieving data, so we just mark account as valid for now
-    res.json({name: `Airtable [****${ending}]`});
+    res.json({name: `Airtable [*****${endingForAccountName}]`});
+});
+
+const getErrorCode = (err) => err.code || err.statusCode || err.status || 500;
+
+const getMessage = (err) => {
+    let message = err.message || err.statusText || 'Ops... Something terrible happened';
+    if (getErrorCode(err) === 401) {
+        message = VALID_API_KEY;
+    }
+    return message;
+};
+
+app.post('/', (req, res) => {
+    const apiKey = req.body.account.key;
+    const baseId = req.body.filter.base;
+    const table = req.body.filter.table;
+    const base = new Airtable({apiKey: apiKey}).base(baseId);
+
+    res.type('json');
+
+    const stream = JSONStream.stringify();
+    stream.pipe(res);
+
+    const onPageRetrieved = (partialRecords, fetchNextPage) => {
+        partialRecords.forEach((row) => {
+            const value = Object.assign({
+                id: row.id,
+
+                'created on': row._rawJson.createdTime
+            }, row.fields);
+            stream.write(value);
+        });
+        fetchNextPage();
+    };
+
+    const onFetchingComplete = (err) => {
+        if (err) {
+            stream.write({
+                __streamError: {
+                    message: getMessage(err),
+                    code: getErrorCode(err)
+                }
+            });
+        }
+        stream.end();
+
+    };
+
+    base(table).select().eachPage(onPageRetrieved, onFetchingComplete);
 });
 
 app.listen(process.env.PORT || 8080);
